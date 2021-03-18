37eeaaea1f80833ae6f891660a300afd61137159
Julien Poissonnier <julien@caffeine.lu>
2021-03-18 15:32:49 +1300

    oh no fix the array

--- src/api/fruit.js
+++ src/api/fruit.js
@@ -1,7 +1,7 @@
 const fruits = ["Banana", "Apple", "Kiwi Fruit"]
 
 export default (req, res) => {
-  const fruit = fruits[Math.floor(Math.random() * array.length)]
+  const fruit = fruits[Math.floor(Math.random() * fruits.length)]
   res.status(200).send({
     fruit,
   })
