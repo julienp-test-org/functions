ce2f1d027331eebdde900d06014168324f878417
Julien Poissonnier <julien@caffeine.lu>
2021-03-18 15:29:27 +1300

    more fruit

--- src/api/fruit.js
+++ src/api/fruit.js
@@ -1,5 +1,8 @@
+const fruits = ["Banana", "Apple", "Kiwi Fruit"]
+
 export default (req, res) => {
+  const fruit = fruits[Math.floor(Math.random() * array.length)]
   res.status(200).send({
-    fruit: "Banana",
+    fruit,
   })
 }
