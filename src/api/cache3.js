31e2875dd1803ccf4fd97a4744cd8b690bdf54d7
Julien Poissonnier <julien@caffeine.lu>
2021-06-07 16:10:34 +0200

    eek

--- src/api/cache3.js
+++ src/api/cache3.js
@@ -1,7 +1,7 @@
 export default (req, res) => {
   const time = new Date().getTime()
   if (time % 2 === 0) {
-    res.set(`Cache-Control`, `public, s-maxage=120`)
+    res.set(`Cache-Control`, `public, max-age=60`)
     res.status(500).send({
       time: new Date().getTime(),
     })
