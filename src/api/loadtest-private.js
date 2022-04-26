0b301afe714cec0532b1211a79e75ed76f2a826d
Julien Poissonnier <julien@caffeine.lu>
2022-04-26 10:40:51 +0200

    Add load test function

---
+++ src/api/loadtest-private.js
@@ -0,0 +1,25 @@
+function handler(req, res) {
+  let primes = getPrimes(10000)
+  res.set(`Cache-Control`, `private`)
+  res.json(primes)
+}
+
+function getPrimes(max) {
+  var sieve = [],
+    i,
+    j,
+    primes = []
+
+  for (i = 2; i <= max; ++i) {
+    if (!sieve[i]) {
+      // i has not been marked -- it is prime
+      primes.push(i)
+
+      for (j = i << 1; j <= max; j += i) {
+        sieve[j] = true
+      }
+    }
+  }
+
+  return primes
+}
